7b0248754237d39468731586a5b9dcd7aa63b17d
root <root@cent8.localdomain>
2019-11-17 20:42:04 -0700

    more

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Mike'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Denver!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'Mr. Robot is so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
